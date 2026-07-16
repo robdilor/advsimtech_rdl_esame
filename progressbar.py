9cbe62a374d6ea09fc55d87825a1738805c4ad2f
Roberto Di Loreto <diloretoroberto@icloud.com>
2026-07-16 16:43:51 +0200

    fix: 16/07/2026

--- progressbar.py
+++
@@ -1,6 +0,0 @@
-def progress_bar(step, total):
-    step = max(0, min(step, total))
-    bar = '██' * step + '░░' * (total - step)
-    percentage = step / total if total else 0
-    print(f'\r{bar} {percentage:.2%}', end='', flush=True)
-    
